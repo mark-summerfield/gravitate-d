2e60a846f6f474b833fd4af62e946a97119dc362
mark-summerfield <mark@qtrac.eu>
2020-02-13 17:16:02 +0000

    minor

--- src/app.d
+++ src/app.d
@@ -1,14 +1,25 @@
 #!/usr/bin/env dub
 // Copyright © 2020 Mark Summerfield. All rights reserved.
-import gtk.Main;
+import gtk.Main: Main;
+import gtk.MainWindow: MainWindow;
+import gtk.Widget: Widget;
 import std.stdio;
 
 void main(string[] args) {
-    import gtk.MainWindow: MainWindow;
+    Main.init(args);
+    auto game = new GameWindow();
+    Main.run();
+}
 
-	Main.init(args);
-	MainWindow mainWindow = new MainWindow("Gravitate");
-	mainWindow.addOnDestroy(delegate void(Widget) { Main.quit(); } );
-	mainWindow.showAll();
-	Main.run();
+private class GameWindow : MainWindow {
+    this() {
+        super("Gravitate");
+        // TODO
+        addOnDestroy(&quit);
+        showAll();
+    }
+
+    void quit(Widget widget) {
+        Main.quit();
+    }
 }
