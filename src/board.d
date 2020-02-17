a9fcf68b9e177b47ec6db769a9a492aaadaa74d5
mark-summerfield <mark@qtrac.eu>
2020-02-17 09:04:10 +0000

    got UI working
    - not happy with button layout
    - all sizes are fixed instead of user-defined
    - board does nothing

---
+++ src/board.d
@@ -0,0 +1,10 @@
+// Copyright © 2020 Mark Summerfield. All rights reserved.
+
+import gtk.DrawingArea: DrawingArea;
+
+final class Board: DrawingArea {
+
+    this() {
+    }
+    // TODO
+}
--- src/gamewindow.d
+++ src/gamewindow.d
@@ -3,45 +3,94 @@
 import gtk.ApplicationWindow: ApplicationWindow;
 
 final class GameWindow: ApplicationWindow {
+    import board: Board;
     import gtk.Application: Application;
+    import gtk.Button: Button;
+    import gtk.Label: Label;
     import gtk.Widget: Widget;
+
+    private Button newButton;
+    private Button optionsButton;
+    private Button helpButton;
+    private Button aboutButton;
+    private Button quitButton;
+    private Board board;
+    private Label statusLabel;
 
     this(Application application) {
         import common: APPNAME, ICON;
-        import gtk.Box : Box;
-        import gtk.Button: Button;
-        import gtkc.gtktypes : GtkOrientation;
 
         super(application);
-
         setTitle(APPNAME);
         setIconFromFile(ICON); // TODO embed
+        makeWidgets();
+        makeLayout();
+        makeBindings();
 
-        // widgets
-        auto newButton = new Button("_New");
-        auto aboutButton = new Button("_About");
-        auto quitButton = new Button("_Quit");
-        
-        // layout
-        auto hbox = new Box(GtkOrientation.HORIZONTAL, 5);
-        hbox.packStart(newButton, false, false, 5);
-        hbox.packStart(aboutButton, false, false, 5);
-        hbox.packEnd(quitButton, false, false, 5);
-        add(hbox);
+        int width = 400;
+        int height = 400;
+        setSizeRequest(width, height);
+        getSize(width, height);
+import std.stdio: writefln; writefln("this: load size/pos/hiscore %sx%s",
+width, height);
+        board.setSizeRequest(width, height - 40);
 
-        // bindings
-        newButton.addOnClicked(delegate void(Button) { onNew(); });
-        aboutButton.addOnClicked(delegate void(Button) { onAbout(); });
-        quitButton.addOnClicked(delegate void(Button) { close(); });
-        addOnDestroy(&onQuit);
-
-        setSizeRequest(400, 400); // TODO load size/pos
         showAll();
     }
 
+    void makeWidgets() {
+        import gtkc.gtktypes : StockID;
+
+        newButton = new Button(StockID.NEW);
+        // TODO keep the preferences icon but change the text to "_Options"
+        optionsButton = new Button(StockID.PREFERENCES);
+        helpButton = new Button(StockID.HELP);
+        aboutButton = new Button(StockID.ABOUT);
+        quitButton = new Button(StockID.QUIT);
+        // TODO get the board size based on window size
+        board = new Board();
+        statusLabel = new Label("0/0");
+    }
+
+    void makeLayout() {
+        import gtk.Box : Box;
+        import gtk.Separator : Separator;
+        import gtkc.gtktypes : GtkOrientation;
+
+        enum pad = 5;
+        auto hbox = new Box(GtkOrientation.HORIZONTAL, pad);
+        hbox.setHomogeneous(true);
+        hbox.packStart(newButton, false, true, pad);
+        hbox.packStart(optionsButton, false, true, pad);
+        hbox.packStart(helpButton, false, true, pad);
+        hbox.packStart(aboutButton, false, true, pad);
+        hbox.packEnd(quitButton, false, true, pad);
+        auto vbox = new Box(GtkOrientation.VERTICAL, pad);
+        vbox.packStart(hbox, false, true, pad);
+        vbox.packStart(board, true, true, pad);
+        vbox.packEnd(statusLabel, false, true, pad);
+        add(vbox);
+    }
+
+    void makeBindings() {
+        newButton.addOnClicked(delegate void(Button) { onNew(); });
+        optionsButton.addOnClicked(delegate void(Button) { onOptions(); });
+        helpButton.addOnClicked(delegate void(Button) { onHelp(); });
+        aboutButton.addOnClicked(delegate void(Button) { onAbout(); });
+        quitButton.addOnClicked(delegate void(Button) { close(); });
+        addOnDestroy(&onQuit);
+    }
+
     void onNew() {
-        import std.stdio: writeln;
-        writeln("onNew");
+        import std.stdio: writeln; writeln("onNew");
+    }
+
+    void onOptions() {
+        import std.stdio: writeln; writeln("onOptions");
+    }
+
+    void onHelp() {
+        import std.stdio: writeln; writeln("onHelp");
     }
 
     void onAbout() {
@@ -50,8 +99,7 @@ final class GameWindow: ApplicationWindow {
     }
 
     void onQuit(Widget) {
-        import std.stdio: writeln;
-        writeln("onQuit: save size/pos");
+        import std.stdio: writeln; writeln("onQuit: save size/pos");
 
         destroy();
     }
