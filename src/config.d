b7e00433cbf3cd71c6e67b247c06d3c4d345dab4
mark-summerfield <mark@qtrac.eu>
2020-02-22 11:13:48 +0000

    config with Gtk KeyFile

--- src/config.d
+++ src/config.d
@@ -10,7 +10,21 @@ int y = 0;
 int width = 400;
 int height = 400;
 
-private string filename;
+private {
+    enum BOARD = "Board";
+    enum WINDOW = "Window";
+    enum MAXCOLORS = "maxColors";
+    enum COLUMNS = "columns";
+    enum ROWS = "rows";
+    enum DELAYMS = "delayMs";
+    enum HIGHSCORE = "highScore";
+    enum X = "x";
+    enum Y = "y";
+    enum WIDTH = "width";
+    enum HEIGHT = "height";
+
+    string filename;
+}
 
 void initialize(string applicationId) {
     import glib.Util: gutil = Util;
@@ -24,13 +38,32 @@ void initialize(string applicationId) {
 
 void load() {
     assert(filename.length);
-    // TODO load & update or use defaults if no .ini file found
-import std.stdio: writeln; writeln("TODO config.load ", filename); // TODO
+
+    import glib.KeyFile: KeyFile;
+
+    auto keyFile = new KeyFile;
+    if (!keyFile.loadFromFile) {
+        import std.stdio: stderr;
+        stderr.writeln("failed to load config");
+        return;
+    }
+    int n;
+    n = keyFile.getInteger(BOARD, MAXCOLORS);
+    // TODO check if error & if ok set
+    n = keyFile.getInteger(BOARD, COLUMNS);
+    n = keyFile.getInteger(BOARD, ROWS);
+    n = keyFile.getInteger(BOARD, DELAYMS);
+    n = keyFile.getInteger(BOARD, HIGHSCORE);
+    n = keyFile.getInteger(WINDOW, X);
+    n = keyFile.getInteger(WINDOW, Y);
+    n = keyFile.getInteger(WINDOW, WIDTH);
+    n = keyFile.getInteger(WINDOW, HEIGHT);
 }
 
 void save() {
     assert(filename.length);
 
+    import glib.KeyFile: KeyFile;
     import std.file: exists, FileException, mkdirRecurse;
     import std.path: dirName;
 
@@ -40,10 +73,21 @@ void save() {
             mkdirRecurse(path);
         } catch (FileException err) {
             import std.stdio: stderr;
-            stderr.writefln("failed to save config: %s", err);
+            stderr.writefln("failed to create config path: %s", err);
             return;
         }
-    // TODO save config & highScore
-
-import std.stdio: writeln; writeln("TODO config.save ", filename); // TODO
+    auto keyFile = new KeyFile;
+    keyFile.setInteger(BOARD, MAXCOLORS, maxColors);
+    keyFile.setInteger(BOARD, COLUMNS, columns);
+    keyFile.setInteger(BOARD, ROWS, rows);
+    keyFile.setInteger(BOARD, DELAYMS, delayMs);
+    keyFile.setInteger(BOARD, HIGHSCORE, highScore);
+    keyFile.setInteger(WINDOW, X, x);
+    keyFile.setInteger(WINDOW, Y, y);
+    keyFile.setInteger(WINDOW, WIDTH, width);
+    keyFile.setInteger(WINDOW, HEIGHT, height);
+    if (!keyFile.saveToFile(filename)) {
+        import std.stdio: stderr;
+        stderr.writeln("failed to save config");
+    }
 }
