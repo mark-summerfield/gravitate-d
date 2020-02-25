69fbfba83fc3d8075cfcaf3d4edc557737000076
mark-summerfield <mark@qtrac.eu>
2020-02-25 19:17:52 +0000

    moved dxpm file to a data dir (better style)

--- src/common.d
+++ src/common.d
@@ -4,4 +4,4 @@ import std.string: splitLines;
 
 enum APPNAME = "Gravitate";
 enum VERSION = "v5.0.2";
-static string[] ICON_XPM = splitLines(import("gravitate.dxpm"));
+static string[] ICON_XPM = splitLines(import("data/gravitate.dxpm"));
