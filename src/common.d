5a8e8e3e683ee0a719d368d8855837073e4ab02d
mark-summerfield <mark@qtrac.eu>
2020-03-12 16:30:58 +0000

    5.0.9

--- src/common.d
+++ src/common.d
@@ -4,5 +4,5 @@ module qtrac.gravitate.common;
 import std.string: splitLines;
 
 enum APPNAME = "Gravitate";
-enum VERSION = "v5.0.8";
+enum VERSION = "v5.0.9";
 static string[] ICON_XPM = splitLines(import("data/gravitate.dxpm"));
